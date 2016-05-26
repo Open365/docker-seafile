3f051b34a1ee72fcfa76eedd853a389d378ddcfd
Carlos Campderrós <carlos.campderros@eyeos.com>
2016-05-26 17:24:18 +0200

    Revert "extract paths in envars in install_seafile.sh"
    This reverts commit 9fd936aa92da1b4d6f4f6fc2524c410e0e0a0cf5.

--- install_seafile.sh
+++ install_seafile.sh
@@ -3,8 +3,7 @@ set -e
 set -x
 set -o pipefail
 
-SEAFILE_DIR="/opt/seafile/seafile-server-${SEAFILE_VERSION}"
-SEAHUB_DIR="$SEAFILE_DIR/seahub"
+SEAHUB_DIR="/opt/seafile/seafile-server-${SEAFILE_VERSION}/seahub"
 
 # backup original seahub's thirdpart folder, because the github clone doesn't
 # have it (and we haven't investigate in how to build it)
@@ -23,13 +22,13 @@ mv /tmp/thirdpart "$SEAHUB_DIR/thirdpart"
 # This is a temporary patch. We use this for allow remote connections of mysql.
 # see https://github.com/shoeper-forks/seafile/commit/36face2972813e2d9e4e0c0115e1ae6aa98321e7
 # this patch is official but not present in v5.0.5 (and v5.0.6 hasn't come out yet)
-patch -p2 -d "$SEAFILE_DIR" -i /opt/patches/seafile/mysql_remote_host.patch
+patch -p2 -d /opt/seafile/seafile-server-${SEAFILE_VERSION} -i /opt/patches/seafile/mysql_remote_host.patch
 
 # when choosing 'use existing db' in setup it tries to re-create the seahub db
 # see https://github.com/haiwen/seafile/pull/1556
-patch -p2 -d "$SEAFILE_DIR" -i /opt/patches/seafile/use_existing_db.patch
+patch -p2 -d /opt/seafile/seafile-server-${SEAFILE_VERSION} -i /opt/patches/seafile/use_existing_db.patch
 
     # Custom patches in seafdav
-patch -d "$SEAHUB_DIR"/thirdpart/ -p1 -i /opt/patches/seafdav/0001-wsgiServer-Reduce-the-socket-poll-time.patch
-patch -d "$SEAHUB_DIR"/thirdpart/ -p1 -i /opt/patches/seafdav/0002-Make-the-webdav-server-scale-better.patch
+patch -d /opt/seafile/seafile-server-${SEAFILE_VERSION}/seahub/thirdpart/ -p1 -i /opt/patches/seafdav/0001-wsgiServer-Reduce-the-socket-poll-time.patch
+patch -d /opt/seafile/seafile-server-${SEAFILE_VERSION}/seahub/thirdpart/ -p1 -i /opt/patches/seafdav/0002-Make-the-webdav-server-scale-better.patch
 
